f345f93e50eed0825602fd425466833e63391999
ssanjose <ssanjose@academic.rrc.ca>
2022-03-11 17:39:51 -0600

    Adds validation to dates in flights get route

--- src/api/validations/date.validation.js
+++ src/api/validations/date.validation.js
@@ -3,3 +3,22 @@ export const validateDate = (date) => {
     const re = /^\d{4}-\d{2}-\d{2}$/;
     return re.test(date);
 };
+
+export const validateStartEndDates = (startDate, endDate) => {
+    if (!startDate)
+        return "startDate is empty";
+    if (!endDate)
+        return "endDate is empty";
+
+    // validate start and end dates from query using validateDate
+    if (!validateDate(startDate))
+        return "startDate format is invalid";
+    if (!validateDate(endDate))
+        return "endDate format is invalid";
+
+    // end date must be after start date
+    if (startDate > endDate)
+        return "endDate cannot be before startDate";
+
+    return "";
+};
