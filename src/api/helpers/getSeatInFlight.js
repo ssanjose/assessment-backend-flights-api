65045b5a790f8cddaa2b1911097b1c39528a9cae
ssanjose <ssanjose@academic.rrc.ca>
2022-03-11 00:53:50 -0600

    Fixes unique seats each flight

--- src/api/helpers/getSeatInFlight.js
+++ src/api/helpers/getSeatInFlight.js
@@ -1,4 +1,16 @@
 // checks seat within a flight
-export const checkSeatInFlight = (flights, flightNumber, seatNumber) => {
-    return flights.get(flightNumber)["seats"].includes(seatNumber);
+export const checkSeatInFlight = (flights, tickets, flightNumber, seatNumber) => {
+    // check if flight exists
+    let flight = flights.get(flightNumber);
+    let exist = false;
+
+    if (flight) {
+        flight["seats"].forEach(seat => {
+            let ticket = tickets.get(seat);
+            if (ticket && ticket["seatNumber"] === seatNumber)
+                exist = true;
+        });
+    }
+
+    return exist;
 };
