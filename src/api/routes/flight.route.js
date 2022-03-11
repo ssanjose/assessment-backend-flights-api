c644b09b2d2aeaeecb756d303e887e23eb9e5748
ssanjose <ssanjose@academic.rrc.ca>
2022-03-11 17:40:10 -0600

    Implements flights get route

--- src/api/routes/flight.route.js
+++ src/api/routes/flight.route.js
@@ -1,2 +1,29 @@
 import express from 'express';
+import { getAllFlights } from '../controllers/flight.controller';
+import { validateStartEndDates } from '../validations/date.validation';
 export const flight = express.Router();
+
+// Flight routes
+flight.get('/', (req, res) => {
+    try {
+        let flights = getAllFlights(req);
+        console.log(flights);
+        // check if status is success or failed
+        if (flights.statusMessage === "success") {
+            res.status(200).json(flights.data);
+        }
+        else {
+            res.status(400).json({
+                statusMessage: "failed",
+                reason: flights.data
+            });
+        }
+    }
+    catch (e) {
+        console.log(e);
+        res.status(500).json({
+            status: "failed",
+            reason: "Internal server error"
+        });
+    }
+});
