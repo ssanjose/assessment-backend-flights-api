8585a2aa98ab772732dc4294de40c3cfe65e6306
ssanjose <ssanjose@academic.rrc.ca>
2022-03-11 01:19:55 -0600

    Change ticket post route method

--- src/api/routes/ticket.route.js
+++ src/api/routes/ticket.route.js
@@ -1,10 +1,30 @@
 import express from 'express';
+import { ticketPost } from '../controllers/ticket.controller';
 export const ticket = express.Router();
 
 // Ticket routes
+// req.app.locals.tickets is a Map
 ticket.post('/', (req, res) => {
-    res.json({
-        status: 'success',
-        message: 'Ticket created'
-    });
+    try {
+        let ticketMessage = ticketPost(req);
+
+        if (ticketMessage === "") {
+            res.status(200).json({
+                status: "success",
+            });
+        }
+        else {
+            res.status(400).json({
+                status: "failed",
+                reason: ticketMessage
+            });
+        }
+    }
+    catch (e) {
+        console.log(e);
+        res.status(500).json({
+            status: "failed",
+            reason: "Internal server error"
+        });
+    }
 });
