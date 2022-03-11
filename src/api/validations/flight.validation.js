5a909b175230ca36a240004a7d2557464448cc22
ssanjose <ssanjose@academic.rrc.ca>
2022-03-11 01:21:34 -0600

    Removes validation codes in js file and put into txt file

--- src/api/validations/flight.validation.js
+++ src/api/validations/flight.validation.js
@@ -1,11 +1,5 @@
 // validate flight
 export const validateFlight = (flights, event) => {
-    /* flight codes
-        0 - flight does not exist
-        1 - flight date exists and has changed
-        2 - flight date exists and has not changed
-    */
-
     // get flight from flight map
     let flight = flights.get(event["flightNumber"]);
 
@@ -15,5 +9,5 @@ export const validateFlight = (flights, event) => {
     if (flight["flightDate"] !== event["flightDate"])
         return 1;
 
-    return 2;
+    return flight;
 };
--- src/api/validations/ticket.validation.js
+++ src/api/validations/ticket.validation.js
@@ -2,18 +2,6 @@ import { checkSeatInFlight } from "../helpers/getSeatInFlight";
 
 // validates ticket data
 export const validateTicket = (flights, tickets, event) => {
-    /* ticket codes
-        0 - ok
-        1 - ticketId is required
-        2 - flightNumber is required
-        3 - seatNumber is required
-        4 - ticketCost is required
-        5 - ticketCost is not a number
-        6 - ticketId already exists
-        7 - seatNumber is not alpha numeric
-        8 - seatNumber already exists in flight
-    */
-
     // check if ticketId, flightNumber, seatNumber, ticketCost exist
     // check if ticketCost is numeric
     if (!event["ticketId"])
@@ -24,7 +12,7 @@ export const validateTicket = (flights, tickets, event) => {
         return "seatNumber is required";
     if (!event["ticketCost"])
         return "ticketCost is required";
-    if (!isNaN(event["ticketCost"]))
+    if (isNaN(event["ticketCost"]))
         return "ticketCost is not a number";
 
     // check if ticketId is already exists
@@ -38,7 +26,7 @@ export const validateTicket = (flights, tickets, event) => {
 
     // check if seatNumber is unique per flight
     // if it is not, return it already exists
-    if (checkSeatInFlight(flights, event["flightNumber"], event["seatNumber"]))
+    if (checkSeatInFlight(flights, tickets, event["flightNumber"], event["seatNumber"]))
         return "seatNumber already taken";
 
     return "";
